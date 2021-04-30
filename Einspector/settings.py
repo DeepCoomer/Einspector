4db5999e99f01f14e58220ab425a009ecb4c08e6
DeepCoomer <deepcoomer198@gmail.com>
2021-04-30 13:43:03 +0530

    Final Commit

--- Einspector/settings.py
+++ Einspector/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["e-inspector.herokuapp.com"]
+ALLOWED_HOSTS = ["e-inspector.herokuapp.com","*"]
 
 
 # Application definition
